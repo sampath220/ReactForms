34e245a09086ef74e7f553323e554ad622d9cef0
“sampath220 <sampath.jakkireddy@ggktech.com>
2019-08-14 18:14:14 +0530

    msg

---
+++ precache-manifest.56d990605319564c3e575f576f881f78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d9234eea46f6167a36f7395f1632a5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa7a1929331b39168b72",
+    "url": "/static/css/main.64b747a5.chunk.css"
+  },
+  {
+    "revision": "f8dca6269735d096868e",
+    "url": "/static/js/2.ccd082fe.chunk.js"
+  },
+  {
+    "revision": "fa7a1929331b39168b72",
+    "url": "/static/js/main.51093eaf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
